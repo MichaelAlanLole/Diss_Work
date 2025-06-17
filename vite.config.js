c4cac50932b16c9e29a43800b100d2c15dfbb06f
MichaelAlanLole <michael2.lole@live.uwe.ac.uk>
2025-06-17 17:13:19 +0100

    d

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  base: '/Diss_Work/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        second: resolve(__dirname, 'home.html'),
+        third: resolve(__dirname, 'interactionTwo.html')
+        // add more pages if you have them
+      }
+    }
+  }
+})
